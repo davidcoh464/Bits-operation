a0abba889fe2459d823ff1c3028d278434bd1bdd
David Cohen <73124192+davidcoh464@users.noreply.github.com>
2022-08-19 16:25:27 +0300

    rename class

--- SortByBits.h
+++ SortByBits.h
@@ -1,9 +1,5 @@
-#pragma once
-#include<iostream>
-using namespace std;
-
-class SortByBits {
-    void sortBits(int* arr, int start, int end, int bitOn) {
+class BitsSort {
+    void binarySort(int* arr, int start, int end, int bitOn) {
         if (end - start <= 0) // If the array has only one or less values
             return;
         if (bitOn == 0) // If we have already checked all the bits
@@ -21,12 +17,12 @@ class SortByBits {
         }
         bitOn >>= 1;
         //Sorts all numbers from 'start' until 'replace' that 'bitOn' is 0
-        sortBits(arr, start, replace, bitOn);
+        binarySort(arr, start, replace, bitOn);
         //Sorts all numbers from 'replace + 1' until the 'end' that 'bitOn' is 1
-        sortBits(arr, replace + 1, end, bitOn);
+        binarySort(arr, replace + 1, end, bitOn);
     }
 public:
-    void sortBits(int* arr, int size) {
+    void binarySort(int* arr, int size) {
         int endNeg = -1;
         // Moves all the negatives numbers to the start of the array
         for (int i = size - 1; i > 0 && i > endNeg;--i) {
@@ -38,8 +34,8 @@ public:
             }
         }
         //Sorting the negative numbers that are from place 0 until 'endNeg' place
-        sortBits(arr, 0, endNeg, 0x40000000);
+        binarySort(arr, 0, endNeg, 0x40000000);
         //Sorting the non negative numbers that are from place 'endNeg' + 1 until the end
-        sortBits(arr, endNeg + 1, size - 1, 0x40000000);
+        binarySort(arr, endNeg + 1, size - 1, 0x40000000);
     }
 };
