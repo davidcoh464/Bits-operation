66ea9ca8ce12079a8648ad93c65f9ce65dd89974
David Cohen <73124192+davidcoh464@users.noreply.github.com>
2022-08-19 16:19:22 +0300

    change in random
    change the random to be with more wide range

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <ctime>
 #include <chrono>
-#include"BitsOperation.h"
-#include"StandardOperation.h"
-#include"SortByBits.h"
-#include"RegularSort.h"
+#include <random>
+#include "BitsOperation.h"
+#include "BitsSort.h"
+#include "StandardOperation.h"
+#include "RegularSort.h"
+
 using namespace std;
 using namespace std::chrono;
 
@@ -15,14 +17,19 @@ const double ROUND_MULT = 10000;
 const double ROUND_SORT = 10;
 const double ROUND_DIV = 10000;
 
-long int getRand();
+random_device dev;
+mt19937 rng(dev());
+uniform_int_distribution<int> dist(INT_MIN, INT_MAX);
+
+int getRand();
+int getRandWideRange();
 void checkMult();
 void checkSortArray();
 void checkDiv();
 
 int main()
 {
-    srand(time(NULL));
+    srand(time(nullptr));
     checkSortArray();
     checkMult();
     checkDiv();
@@ -31,8 +38,8 @@ int main()
 
 void checkSortArray() {
     steady_clock::time_point start, stop;
-    int countBit = 0, countMerga = 0, countQuick = 0;
-    SortByBits* sb = new SortByBits();
+    int countBinary = 0, countMerga = 0, countQuick = 0;
+    BitsSort* sb = new BitsSort();
     RegularSort* sr = new RegularSort();
     for (int round = 0; round < ROUND_SORT;++round) {
         int size = rand();
@@ -40,21 +47,18 @@ void checkSortArray() {
         int* arr2 = new int[size];
         int* arr3 = new int[size];
         for (int i = 0;i < size;i++) {
-            arr1[i] = arr2[i] = arr3[i] = getRand();
+            arr1[i] = arr2[i] = arr3[i] = getRandWideRange();
         }
 
-
         start = CLOCK::now();
-        sb->sortBits(arr1, size);
+        sb->binarySort(arr1, size);
         stop = CLOCK::now();
-        countBit += duration_cast<milliseconds>(stop - start).count();
-
+        countBinary += duration_cast<milliseconds>(stop - start).count();
 
         start = CLOCK::now();
         sr->mergeSort(arr2, 0, size - 1);
         stop = CLOCK::now();
         countMerga += duration_cast<milliseconds>(stop - start).count();
-
 
         start = CLOCK::now();
         sr->quickSort(arr3, 0, size - 1);
@@ -63,14 +67,14 @@ void checkSortArray() {
 
         for (int i = 0;i < size;++i)
         {
-            assert(arr1[i] == arr2[i]);
+            assert(arr1[i] == arr2[i] && arr1[i] == arr3[i]);
         }
         delete[] arr1;
         delete[] arr2;
         delete[] arr3;
     }
     cout << "Avarage time for one sort in milliseconds:\n";
-    cout << "Bits sort:   " << countBit / ROUND_SORT << endl;
+    cout << "Binary sort: " << countBinary / ROUND_SORT << endl;
     cout << "Merga sort:  " << countMerga / ROUND_SORT << endl;
     cout << "Quick sort:  " << countQuick / ROUND_SORT << endl << endl;
 }
@@ -78,7 +82,7 @@ void checkSortArray() {
 void checkMult() {
     int a, b, resultMsb, resultAdd, resultStd, resultLsb;
     steady_clock::time_point start, stop;
-    int countAdd = 0, countMsb = 0, countStd = 0, countLsb = 0;
+    int countAdd = 0, countStd = 0, countMsb = 0, countLsb = 0;
     for (int i = 0;i < ROUND_MULT;++i) {
         a = getRand();
         b = getRand();
@@ -91,32 +95,32 @@ void checkMult() {
         start = CLOCK::now();
         resultAdd = StandardOperation().multByAdding(a, b);
         stop = CLOCK::now();
-        countAdd += duration_cast<microseconds>(stop - start).count();
+        countAdd += duration_cast<nanoseconds>(stop - start).count();
 
         start = CLOCK::now();
-        resultMsb = BitsOperation().bitsMultByMsb(a, b);
+        resultMsb = BitsOperation().multByMsb(a, b);
         stop = CLOCK::now();
         countMsb += duration_cast<microseconds>(stop - start).count();
 
         start = CLOCK::now();
-        resultLsb = BitsOperation().bitsMultByLsb(a, b);
+        resultLsb = BitsOperation().multByLsb(a, b);
         stop = CLOCK::now();
         countLsb += duration_cast<microseconds>(stop - start).count();
 
-        assert(resultStd == resultMsb && resultStd == resultAdd && resultStd == resultLsb);
+        assert(resultStd == resultMsb && resultStd == resultLsb && resultStd == resultAdd);
     }
     cout << "Avarage time for one multiplication in microseconds:\n";
     cout << "Standard multiply:    " << countStd / ROUND_MULT << endl;
     cout << "Bits multiply by lsb: " << countLsb / ROUND_MULT << endl;
     cout << "Bits multiply by msb: " << countMsb / ROUND_MULT << endl;
-    cout << "Multiply by adding:   " << countAdd / ROUND_MULT << endl<<endl;
+    cout << "Multiply by adding:   " << countAdd / ROUND_MULT << endl << endl;
 }
 void checkDiv() {
-    int a, b, resultWay1, resultWay2, resultStd;
+    int a, b, resultWay1, resultWay2, resultSub, resultStd;
     steady_clock::time_point start, stop;
-    int countWay1 = 0, countWay2 = 0, countStd = 0;
+    int countWay1 = 0, countWay2 = 0, countStd = 0, countSub = 0;
     for (int i = 0;i < ROUND_DIV;++i) {
-        a = getRand();
+        a = getRandWideRange();
         b = getRand();
         if (b == 0)
             continue;
@@ -127,24 +131,35 @@ void checkDiv() {
         countStd += duration_cast<microseconds>(stop - start).count();
 
         start = CLOCK::now();
-        resultWay1 = BitsOperation().divisionWay1(a, b);
+        resultSub = StandardOperation().divideBySubtraction(a, b);
+        stop = CLOCK::now();
+        countSub += duration_cast<microseconds>(stop - start).count();
+
+        start = CLOCK::now();
+        resultWay1 = BitsOperation().divideWay1(a, b);
         stop = CLOCK::now();
         countWay1 += duration_cast<microseconds>(stop - start).count();
 
         start = CLOCK::now();
-        resultWay2 = BitsOperation().divisionWay2(a, b);
+        resultWay2 = BitsOperation().divideWay2(a, b);
         stop = CLOCK::now();
         countWay2 += duration_cast<microseconds>(stop - start).count();
 
-        assert(resultStd == resultWay1 && resultStd == resultWay2);
+        assert(resultStd == resultSub);
+        assert(resultStd == resultWay2);
+        assert(resultStd == resultWay1);
     }
     cout << "Avarage time for one division in microseconds:\n";
-    cout << "Standard division:   " << countStd / ROUND_DIV << endl;
-    cout << "Bits division way 1: " << countWay1 / ROUND_DIV << endl;
-    cout << "Bits division way 2: " << countWay2 / ROUND_DIV << endl;
+    cout << "Standard division:       " << countStd / ROUND_DIV << endl;
+    cout << "Bits division way 1:     " << countWay1 / ROUND_DIV << endl;
+    cout << "Bits division way 2:     " << countWay2 / ROUND_DIV << endl;
+    cout << "Division by subtraction: " << countSub / ROUND_DIV << endl;
 }
 
-long int getRand() {
-    return rand() - rand();
+int getRandWideRange() {
+    return dist(rng);
+}
+int getRand() {
+    return rand() % 3 ? rand() : -rand();
 }
 
